5d067c13abc4b59826e85ede8c8bc35c50e5b755
Yzhak Zalmom <yzhakzalmom@gmail.com>
2025-11-27 16:18:30 -0300

    feat: cria função replace_caractere_decimal e aplica em SG_clean_sales

---
+++ src/akira/utils.py
@@ -0,0 +1,13 @@
+# Troca ponto por vírgula nas colunas float64 em um dataframe
+def replace_caractere_decimal(df):
+
+    # Para cada coluna
+    for column in df.columns:
+
+        # Caso a coluna seja float64
+        if df[column].dtype == 'float64':
+
+            # Realiza o replace
+            df[column] = df[column].fillna(0).astype(str).str.replace('.', ',', regex=False)
+
+    return df
