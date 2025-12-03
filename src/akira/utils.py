d56c2392038123b496fb73bf7659d5abaa3b9faa
Yzhak Zalmom <yzhakzalmom@gmail.com>
2025-12-03 09:39:46 -0300

    feat: cria função que agrega planilhas de venda em um csv

--- src/akira/utils.py
+++ src/akira/utils.py
@@ -1,5 +1,7 @@
+import pandas as pd
+
 # Troca ponto por vírgula nas colunas float64 em um dataframe
-def replace_caractere_decimal(df):
+def replace_caractere_decimal(df: pd.DataFrame) -> pd.DataFrame:
 
     # Para cada coluna
     for column in df.columns:
@@ -11,3 +13,29 @@ def replace_caractere_decimal(df):
             df[column] = df[column].fillna(0).astype(str).str.replace('.', ',', regex=False)
 
     return df
+
+
+# Une os dataframes de vendas em bronze/vendas
+def une_vendas():
+    from pathlib import Path
+
+    # Define diretório de vendas
+    diretorio_vendas = '../data/silver/separate_sales'
+
+    # Define uma variável que armazenará o dataframe final
+    vendas_df = None
+
+    # Itera pelos arquivos do diretório de vendas como objetos
+    for item in Path(diretorio_vendas).iterdir():
+
+        # Garante que o item seja um arquivo
+        if item.is_file():
+
+            # Lê o arquivo como DataFrame
+            df = pd.read_excel(item)
+
+            # Une o dataframe com o dataframe final
+            vendas_df = pd.concat([vendas_df, df])
+
+    # Salva o dataframe final como CSV
+    vendas_df.to_csv('../data/silver/union_sales/sales.csv', index=False)
