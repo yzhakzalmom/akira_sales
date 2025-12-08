2a66153324a6978ad1d2e796e68ccadbffdb14ca
Yzhak Zalmom <yzhakzalmom@gmail.com>
2025-12-08 17:37:41 -0300

    feat: add containers to the main page

--- app/main.py
+++ app/main.py
@@ -5,26 +5,32 @@ from components import header, sales_uploader
 # Page settings
 st.set_page_config(page_title="Akira ~ Uploads", page_icon="🥋", layout="wide")
 
+# Create main page container
+main_container = st.container()
+
 # Create main page columns
-_, main_col, __ = st.columns([1, 4, 1])
+_, main_col, __ = main_container.columns([1, 4, 1])
+
+# Create central container
+central_container = main_col.container()
 
 # Render within the column
 with main_col:
-    
+
     # Render header
-    header.render_header()
+    header.render_header(central_container)
 
     # Render sales uploader
-    sales_uploader.render_sales_uploader()
+    sales_uploader.render_sales_uploader(central_container)
 
-    st.divider()
+    central_container.divider()
 
     # Lógica para inserir custos com produtos
-    st.title("Custos")
-    st.write("Inserir custos")
+    central_container.title("Custos")
+    central_container.write("Inserir custos")
 
-    st.divider()
+    central_container.divider()
 
     # Lógica para inserir outros custos
-    st.title("Outros Custos")
-    st.write("Inserir outros custos")
+    central_container.title("Outros Custos")
+    central_container.write("Inserir outros custos")
