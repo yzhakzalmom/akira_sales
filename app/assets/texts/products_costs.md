cd600af4aaf5bda817f17e5f7d46c6cbf7852ff4
Yzhak Zalmom <yzhakzalmom@gmail.com>
2025-12-08 22:04:26 -0300

    feat: create function to read dataframes from assets and fix get_text function

---
+++ app/assets/texts/products_costs.md
@@ -0,0 +1,4 @@
+Aqui você deve:
+1. Preencher cada linha com Descrição, Mês, Ano e Custo
+1. Conferir se todos os valores estão corretos
+1. Enviar informações
--- src/services/read_data.py
+++ src/services/read_data.py
@@ -1,10 +1,23 @@
-def get_sales_uploader_text() -> str:
-    from utils.helpers import get_asset_file_path, get_months
+from __future__ import annotations
+
+def get_text(text_file_name: str) -> str:
+    from utils.helpers import get_asset_file_path
+
     # Find text path
-    text_path = get_asset_file_path('texts', 'sales_uploader.md')
+    text_path = get_asset_file_path('texts', f'{text_file_name}.md')
 
     # Read sales uploader text
     with open(text_path, 'r', encoding='utf-8') as f:
         text = f.read()
 
     return text
+
+def get_dataframe(df_name: str) -> pd.DataFrame:
+    import pandas as pd
+    from utils.helpers import get_asset_file_path
+
+    # Find df path
+    df_path = get_asset_file_path('dataframes', f'{df_name}.csv')
+
+    # Read and return placeholder csv
+    return pd.read_csv(df_path, sep=';')
