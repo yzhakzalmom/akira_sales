f4301a7b0525c5c04395f3f504ca3d144c75917d
Yzhak Zalmom <yzhakzalmom@gmail.com>
2025-12-13 19:18:48 -0300

    feat: add function read_sheet_bytes and apply on notebooks

--- src/services/read_data.py
+++ src/services/read_data.py
@@ -1,8 +1,17 @@
 from __future__ import annotations
 
 import pandas as pd
+from io import BytesIO
+from services.ADLSClient import ADLSClient
 from utils.constants import *
 from utils.helpers import get_asset_file_path
+
+# Create ADLS connection object
+adls_client = ADLSClient()
+
+# =======================
+# ASSETS
+# =======================
 
 def get_asset_text(text_file_name: str) -> str:
     
@@ -22,3 +31,18 @@ def get_asset_dataframe(df_name: str) -> pd.DataFrame:
 
     # Read and return placeholder csv
     return pd.read_csv(df_path, sep=CSV_SEP)
+
+# =======================
+# LAYERS
+# =======================
+
+def read_sheet_bytes(layer: str, year: str, month: str):
+
+    # Construct the file path using layer, year, and month
+    path = f'{layer}/{ADLS_CATEGORY_SALES}/{year}/{month}'
+
+    # Read the Excel file from ADLS as bytes
+    sheet_bytes = BytesIO(adls_client.read_folder(path))
+
+    # Return the Excel bytes
+    return sheet_bytes
