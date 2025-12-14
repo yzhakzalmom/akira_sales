497bbed998e8e27be80d263f5209a2a6dec86569
Yzhak Zalmom <yzhakzalmom@gmail.com>
2025-12-13 22:27:51 -0300

    feat: create functions read_tabular and save_df and apply on notebooks

--- src/services/ADLSClient.py
+++ src/services/ADLSClient.py
@@ -1,4 +1,5 @@
 import os
+from io import BytesIO
 from utils.constants import MSG_FILE_NOT_FOUND
 from utils.helpers import load_env_file
 from azure.identity import ClientSecretCredential
@@ -45,7 +46,7 @@ class ADLSClient:
 
     def read(self, path: str) -> bytes:
         file_client = self.fs_client.get_file_client(path)
-        return file_client.download_file().readall()
+        return BytesIO(file_client.download_file().readall())
 
     def read_folder(self, folder_path: str) -> bytes:
         paths = self.fs_client.get_paths(path=folder_path)
@@ -53,7 +54,7 @@ class ADLSClient:
         for path in paths:
             if not path.is_directory:
                 file_client = self.fs_client.get_file_client(path.name)
-                return file_client.download_file().readall()
+                return BytesIO(file_client.download_file().readall())
 
         raise FileNotFoundError(f'{MSG_FILE_NOT_FOUND} {folder_path}')
 
