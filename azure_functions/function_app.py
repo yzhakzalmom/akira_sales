0a55ae0431adbb62a9ec75dc2932012431cdc098
Yzhak Zalmom <yzhakzalmom@gmail.com>
2026-03-26 23:24:08 -0300

    build: remove azure_functions folder for now, it needs a separate project

--- azure_functions/function_app.py
+++
@@ -1,8 +0,0 @@
-import azure.functions as func
-from features.monthly_executions import bp as monthly_executions_bp
-
-# Create funcion app object
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-
-# Register blueprint function in the function app
-app.register_functions(monthly_executions_bp)
